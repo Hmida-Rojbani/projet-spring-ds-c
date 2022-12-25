7b1809402561b7b1a0e225458bfcd91b206ccd4a
mahmoud-somai <somai.mahmoud096@gmail.com>
2022-12-25 23:52:21 +0100

    first commit

---
+++ src/main/java/de/tekup/studentsabsence/entities/Group.java
@@ -0,0 +1,99 @@
+package de.tekup.studentsabsence.entities;
+
+import de.tekup.studentsabsence.enums.LevelEnum;
+import de.tekup.studentsabsence.enums.SpecialityEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Entity
+@Data
+@ToString(exclude = "students")
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "_group")
+public class Group {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "Name is required")
+    private String name;
+    @NotBlank(message = "Label is required")
+    private String label;
+    @Enumerated(EnumType.STRING)
+    private LevelEnum level;
+    @NotNull(message = "Speciality is required")
+    @Enumerated(EnumType.STRING)
+    private SpecialityEnum speciality;
+    //TODO Complete Relations with other entities
+
+
+    @ManyToOne
+    private GroupSubject groupSubject;
+
+
+    @OneToMany
+    private List<Student> students;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public LevelEnum getLevel() {
+        return level;
+    }
+
+    public void setLevel(LevelEnum level) {
+        this.level = level;
+    }
+
+    public SpecialityEnum getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(SpecialityEnum speciality) {
+        this.speciality = speciality;
+    }
+
+    public GroupSubject getGroupSubject() {
+        return groupSubject;
+    }
+
+    public void setGroupSubject(GroupSubject groupSubject) {
+        this.groupSubject = groupSubject;
+    }
+
+    public List<Student>  getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+}
---
+++ src/main/java/de/tekup/studentsabsence/services/StudentService.java
@@ -0,0 +1,19 @@
+package de.tekup.studentsabsence.services;
+
+import de.tekup.studentsabsence.entities.Student;
+
+import java.util.List;
+
+public interface StudentService {
+    List<Student> getAllStudents();
+
+    Student getStudentBySid(Long sid);
+
+    Student addStudent(Student student);
+
+    Student updateStudent(Student student);
+
+    Student deleteStudent(Long sid);
+
+
+}
