84383e18ce4b9774ac470c462ce72ba343191014
neyssenbr <48225357+neyssenbr@users.noreply.github.com>
2022-12-26 01:56:05 +0100

    Update Student.java

--- src/main/java/de/tekup/studentsabsence/entities/Student.java
+++ src/main/java/de/tekup/studentsabsence/entities/Student.java
@@ -39,5 +39,81 @@ public class Student implements Serializable {
     {
         private Image image;
     }
+    @OneToMany
+    @JoinColumn(name="startDate")
+    {
+    private List<Absence> absences;
+    }
+    
+ public String getSid() {
+        return sid;
+    }
 
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public List<Absence> getAbsences() {
+        return absences;
+    }
+
+    public void setAbsences(List<Absence> absences) {
+        this.absences = absences;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
 }
