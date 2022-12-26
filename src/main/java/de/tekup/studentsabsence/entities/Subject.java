65a1891fb881bd690b8f3ad172d2d106a6156dbf
neyssenbr <48225357+neyssenbr@users.noreply.github.com>
2022-12-26 02:01:53 +0100

    Update Subject.java

--- src/main/java/de/tekup/studentsabsence/entities/Subject.java
+++ src/main/java/de/tekup/studentsabsence/entities/Subject.java
@@ -27,4 +27,20 @@ public class Subject implements Serializable {
 
     @ManyToMany(mappedBy = "Subject")
     private List<Group> groupList;
+   
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
